15743bec955bc03f2e260b5c4ae310810e571bcc
muhammed ajmal <muhammedajmalcc6424094@gmail.com>
2024-10-18 16:21:51 +0530

    card completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,59 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { styles } from '../styles'
-import { ComputersCanvas } from './canvas'
-
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
-      {/* Container for hero content */}
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] mx-auto flex flex-col md:flex-row items-start gap-5`}>
-        
-        {/* Left side content */}
-        <div className='flex flex-col justify-center items-center md:items-center'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
-        {/* Right side content */}
-        <div className='flex flex-col justify-center items-center md:items-start p-5 md:p-10'>
-          <h1 className={`${styles.heroHeadText} text-white text-center md:text-left`}>
-            Hi, I'm <span className='text-[#915eff]'>Muhammed Ajmal</span>
+        <div>
+          <h1
+            className={`${styles.heroHeadText}
+     text-white`}
+          >
+            hi, I'm <span className="text-[#915eff]">Ajmal</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100 text-center md:text-left`}>
-            I am a MERN Stack Developer
+          <p className={`${styles.heroSubText} mt-2`}>
+            A MERN stack developer builds full-stack web applications using
+            MongoDB, Express.js, React.js, and Node.js. They handle both
+            front-end and back-end development, <br  className="sm:block hidden"/> using JavaScript across the
+            entire stack. MERN developers create scalable, dynamic web
+            applications with efficient data handling and user interfaces.
           </p>
         </div>
       </div>
+     < ComputersCanvas/>
+     <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+     <a href="#about">
+<div className="w-[35px] h-[64px]  rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+  <motion.dev
+  
+  animate={{
+    y:[0,24,0]
+  }}
+  transition={{
+    duration:1.5,
+    repeat:Infinity,
+    repeatType:'loop'
+  
+  }}
+  className="w-2 h-3 rounded-full bg-secondary mb1"
+  />
+    
 
-      {/* 3D Canvas */}
-      <ComputersCanvas />
+</div>
+
+     </a>
+     </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,28 +1,52 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Preload, useGLTF, OrbitControls } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-
-const Computers = () => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+const Computers = ({isMobile}) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
-      <primitive object={computer.scene} 
-      scale={.7}
-      position={[0,-3.25,-1.5]}
+      <pointLight
+        intensity={1}
+        angle={0.12}
+        penumbra={1}
+        castShadow
+        shadow-mapsize={1024}
       />
-      
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7: 0.75}
+        position={ isMobile ?[0,-3,-2.2]:[2, -3.02, -1.5]}
+        rotation={[-0.01, 0.1, -0.1]}
+      />
     </mesh>
   );
 };
 
 export const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // Cleanup listener on component unmount
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
-    <Canvas className=''
-      frameloop='demand'
+    <Canvas
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
@@ -33,7 +57,7 @@ export const ComputerCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers  isMobile={isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
