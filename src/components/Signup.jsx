a67f64218701a29ddeab12659bba78c7ad9f6b7e
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2024-12-23 12:47:04 +0530

    add some changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -47,7 +47,7 @@ import {
         <button
           className="bg-100 relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit">
-          Sign up &rarr;
+        Submit &rarr;
           <BottomGradient />
         </button>
 
@@ -82,16 +82,3 @@ const LabelInputContainer = ({
 
 
 
-
-// import React from 'react';
-
-// const Contact = () => {
-//   return (
-//     <div>
-//       <h1 className='text-red-600'>contect</h1>
-
-//     </div>
-//   );
-// }
-
-// export default Contact;
